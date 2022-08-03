363ef85940b5a6cec329261e7622eaf8829707e7
Huanyu Li <lihuanyu1024@outlook.com>
2022-08-04 01:07:58 +0930

    dance cat with music and color change

--- huanyu-li/week2/wed/js/main.js
+++ huanyu-li/week2/wed/js/main.js
@@ -5,18 +5,15 @@
 
 //There are 2 versions of code. 
 
-//First version is using 'vw' unit to detect the window size: 100vw
+//First version is using 'vw' unit to detect the window size: 100vw is the edge
+
 //Second version is using 'px' unit.
 
-// The cat can dance with music and random background color (1 per second).
+// In this version, the cat can dance with music and random background color change (1 per second).
 
 // Please click the cat to start.
-// Why? Can't play background music without clicking the page
+// Why? Can't play background music without clicking the page first
 // Uncaught (in promise) DOMException: play() failed because the user didn't interact with the document first.
-
-//direction is a boolean: 
-//true: left->right | false: right->left
-
 
 // const danceCat = "https://media0.giphy.com/media/PdKTOwHgOASGY/giphy.gif?cid=ecf05e47gz11f5xx5udwqj9xd022wqmofmrxyyj6wrbajsx1&rid=giphy.gif&ct=g";
 
@@ -26,28 +23,25 @@ const danceCat = "image/danceCat.gif";
 const catUrl = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 const music = document.querySelector('#music');
 
-
 let direction = true;
+//direction is a boolean: 
 //true: left->right | false: right->left
 
 const catWidth = cat.clientWidth;
+// return the image size in 'px'
 
 const initCat = function () {
     cat.style.position = "absolute";
     cat.style.top = "20vh";
     cat.style.left = "0px";
-};
+}();
 
-initCat();
-
-const pxToVw = function (px, windowEdge) {
-    // transfer unit from px to vw
-    return px * 100 / windowEdge;
-}
-
-// initCat();
 //===========cat walk vw version====================
 
+// const pxToVw = function (px, windowEdge) {
+//     // transfer unit from px to vw
+//     return px * 100 / windowEdge;
+// }
 
 // const catWalkVw = function () {
 //     let nextPosition = null;
@@ -85,6 +79,7 @@ const pxToVw = function (px, windowEdge) {
 
 // // window.setInterval(catWalkVw, 50);
 
+
 //============cat walk px version=========
 
 const catWalk = function () {
@@ -93,13 +88,14 @@ const catWalk = function () {
     const currentPosition = parseInt(cat.style.left);
     const edge = windowWidth - catWidth;
     const middle = Math.ceil(edge / 2 / 10) * 10;
+    // to ensure number's step is 10
+    // initial currentPosition value is 0
 
     if (direction && currentPosition < edge) {
         // walk forward
         nextPosition = currentPosition + 10;
     } else if (direction && currentPosition >= edge) {
         // touch or beyond the boundary -> turn around
-        // console.log(currentPosition, catWidthInVw);
         direction = !direction;
         cat.style.transform = "scaleX(-1)";
     } else if (!direction && currentPosition > 0) {
@@ -115,7 +111,7 @@ const catWalk = function () {
         console.log(direction);
     }
 
-
+    // dance function
     if (currentPosition === middle) {
         document.body.style.backgroundColor = colorGen();
         cat.src = danceCat;
@@ -125,7 +121,6 @@ const catWalk = function () {
         window.setTimeout(function () {
             cat.src = catUrl;
             music.pause();
-            // music.currentTime = 0;
             timerId = window.setInterval(catWalk, 50);
             window.clearInterval(timerGB);
             document.body.style.backgroundColor = 'white';
@@ -155,9 +150,8 @@ const button = document.getElementById('btn');
 
 cat.addEventListener('click',function () {
     timerId = window.setInterval(catWalk, 50);
-})
-
-
+}, {once : true})
+// this event listener can only run once, otherwise, a bug will occur
 
 
 
