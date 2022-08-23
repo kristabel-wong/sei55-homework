0dd5e870722c766cebe92c81bd02687db20d6779
Philippa Choong <pippachoong@gmail.com>
2022-08-23 11:46:20 +1000

    updated monday homework

--- philippa-choong/week5/mon/game/app/controllers/rps_controller.rb
+++ philippa-choong/week5/mon/game/app/controllers/rps_controller.rb
@@ -13,12 +13,14 @@ class RpsController < ApplicationController
 
 
         conditions = {
-        "Rock" => "Scissors",
+        "Rock" => "Scissors", 
         "Paper" =>  "Rock",
         "Scissors" => "Paper"
         }
 
-        if conditions[@user_selection] == @comp_select
+        # rules of rock, paper, scissors can be expressed as a simple hash: player vs computer (key:value)
+
+        if conditions[@user_selection] == @comp_select #check if one player won over the other with:
             @report=  "You win! You have selected #{@user_selection} while computer selected #{@comp_select}"
         elsif @user_selection == @comp_select
             @report =  "Its a draw!"
