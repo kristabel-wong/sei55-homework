f7549443b8164bebce5514b216ce55f599744905
Eva Peng <rainpyr@gmail.com>
2022-08-23 10:01:18 +1000

    rails games

---
+++ eva-peng/week5/mon/games/app/controllers/rockpaperscissors_controller.rb
@@ -0,0 +1,19 @@
+class RockpaperscissorsController < ApplicationController
+    def show
+    end
+
+    def play
+        @throw = params[:throw]
+        @computer_throw = ['rock', 'paper', 'scissors'][rand 3]
+        if @throw == @computer_throw
+            @result = "It's a draw." 
+        elsif (@throw == 'rock' && @computer_throw == 'scissors') || (@throw == 'scissors' && @computer_throw == 'paper') || (@throw == 'paper' && @computer_throw == 'rock')
+            @result = "You Won!"
+        else
+            @result = "Computer Won!"
+        end
+
+    end
+
+
+end
