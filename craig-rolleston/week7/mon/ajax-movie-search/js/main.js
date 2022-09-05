104d9300c982e6d8533e424c28d28a23549a739b
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-05 22:46:15 +1000

    second commit

--- craig-rolleston/week7/mon/ajax-movie-search/js/main.js
+++ craig-rolleston/week7/mon/ajax-movie-search/js/main.js
@@ -6,10 +6,47 @@ const lookupMovie = function( movie ){
     // make a new instance - below is the constructor - gets called when an object is created using the new keyword
     const xhr = new XMLHttpRequest(); 
 
-    xhr.onload = function(){
+    xhr.onload = function(){ 
+
         console.log('available response', xhr.response);
 
-        const results = JSON.parse( xhr.response );
-    }
+        const data = JSON.parse( xhr.response );
+        console.log(data);
 
-}
+        movieInfo = data.results //[i].title
+        
+        for (let i = 0; i < movieInfo.length; i++ ){
+            
+            const getData = movieInfo[i]
+            console.log(getData)
+            
+            $('#movieResults').prepend(`<h2>${getData.title}</h2><br>${getData.poster_path}<br>${getData.popularity}<br>${getData.overview}`); // add data to page 
+            
+        }
+
+    }; // onload handler
+    
+    $('#movieResults').empty() // remove previous results 
+
+    xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=${movie}`);
+
+    xhr.send(); // actually send the request
+
+
+}; // lookupMovie
+
+$( function() {
+
+    $('#searchButton').on('click', function(){
+
+        const movieSearch = $('#movieQuery').val(); // gets value typed into the form
+
+        console.log(`Button clicked! Movie is:`, movieSearch);
+
+        lookupMovie( movieSearch ); // invoke lookupMovie, perform request
+
+    })
+
+})
+console.log($('#movieQuery'));
+
