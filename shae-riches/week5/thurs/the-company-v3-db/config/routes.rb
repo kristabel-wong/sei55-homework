5829c61cc5b2b9a23dbc0f1811a63883f28b6c63
Shae Riches <shaeriches@gmail.com>
2022-09-06 09:30:32 +1000

    all hw

--- shae-riches/week5/thurs/the-company-v3-db/config/routes.rb
+++ shae-riches/week5/thurs/the-company-v3-db/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'employees#home'
+  root to: 'the_company#home'
   # ----------------------------------------------------------- #
   #CREATE
   #1
