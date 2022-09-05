6f839d61ec203ddbec3bfab6b01d7f9261334580
Shae Riches <shaeriches@gmail.com>
2022-09-05 20:18:42 +1000

    queryBase done, format in morning

--- shae-riches/week7/mon/js/main.js
+++ shae-riches/week7/mon/js/main.js
@@ -2,21 +2,59 @@
 console.log('js loaded');
 console.log(`${jQuery} loaded`);
 
-require 'httparty'  
-
-// data = HTTParty.get 'https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=YOUR_SPECIFIC_QUERY_TEXT_GOES_HERE'
-
-const queryMovieDatabase = function(params) {
+const queryMovieDatabase = function( movieTitle ) {
     
 
-    
+    $('#results').html('Loading...');   // clears last results, also provides info to user
 
-    https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=aliens
+    const xhr = new XMLHttpRequest();   // make new instance - this is the constructor
+
+    xhr.onload = function(){
+      
+        console.log('onload available response =', xhr.response );
 
 
- 
+        const data = JSON.parse( xhr.response );
         
+
+        $(`#results`)
+            .empty()    //                  remove previous results    
+            .prepend(`
+                <h2>'${ '#movieSearchInput' }' Search Results</h3>
+                ${ for (let i = 0; i < array.length; i++) {
+                    const element = array[i];
+                    
+                    }
+
+
+                }
+                <h4>${ data }</h4>
+
+            `);         //                  output of queryData to index.html page as #results div
+ 
+
+    };    
+    //              Tell XML-HTTP-Request which URL to open and HOW
+    xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=${movieTitle}?json` )
+
+    xhr.send();     //                  SENDS request & does NOT block - unlike Ruby        
     
  
 
-}
+};                  //                  queryMovieDatabase
+
+$( function(){
+
+    $('#submitButton').on('click', function(){
+
+        const userSearchTerm = $('#movieSearchInput').val();    // saves user input into const
+
+        console.log(`Search submitted, user searched:`, userSearchTerm); //   check === true
+        
+
+        queryMovieDatabase(userSearchTerm); // ** CALLS queryMovieDatabase WITH USERINPUT ** 
+
+
+    });             //                  close #submitButton.on('click')
+
+});                 //                  close searchHandler
