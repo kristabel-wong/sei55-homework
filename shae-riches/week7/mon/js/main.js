8a1fea84d59af63a8c5d456033d340edd81f7c38
Shae Riches <shaeriches@gmail.com>
2022-09-05 19:28:49 +1000

    start of hw

---
+++ shae-riches/week7/mon/js/main.js
@@ -0,0 +1,22 @@
+
+console.log('js loaded');
+console.log(`${jQuery} loaded`);
+
+require 'httparty'  
+
+// data = HTTParty.get 'https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=YOUR_SPECIFIC_QUERY_TEXT_GOES_HERE'
+
+const queryMovieDatabase = function(params) {
+    
+
+    
+
+    https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=aliens
+
+
+ 
+        
+    
+ 
+
+}
