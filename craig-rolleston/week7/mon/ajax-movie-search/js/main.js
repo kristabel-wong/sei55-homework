43476a7d2c16d7ca6827b225924797bac13b5fe6
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-06 08:19:37 +1000

    comments added for interpretation

--- craig-rolleston/week7/mon/ajax-movie-search/js/main.js
+++ craig-rolleston/week7/mon/ajax-movie-search/js/main.js
@@ -7,27 +7,36 @@ const lookupMovie = function( movie ){
     const xhr = new XMLHttpRequest(); 
 
     xhr.onload = function(){ 
-
+        // This function's code will run when XHR decides the response has finished loading
         // console.log('available response', xhr.response);
 
         const data = JSON.parse( xhr.response );
         // console.log(data);
 
-        movieInfo = data.results 
-        
+        movieInfo = data.results // declare variable for loop with data from above - use below
+
+        // loop through movie array 
         for (let i = 0; i < movieInfo.length; i++ ){
             
-            const getData = movieInfo[i]
-            console.log(getData.title)
-            
-            $('#movieResults').append(`<a href=""><h2>${getData.title}</h2></a><br><img src="https://image.tmdb.org/t/p/w185${getData.poster_path}"><br><h3>Popularity: ${getData.popularity}</h3><h3>Description:${getData.overview}</h3>`); // add data to page 
+            // const getData = movieInfo[i] 
+            // set variable above to DRY code if needed
+            console.log(movieInfo[i].title)
+            console.log(movieInfo[i].popularity);
 
-        }
+            // add data to page with .append below - title, poster, popularity, overview
+            $('#movieResults').append(
+                `<a href=""><h2>${movieInfo[i].title}</h2></a><br> 
+                <img src="https://image.tmdb.org/t/p/w185${movieInfo[i].poster_path}"><br> <h3>Popularity: ${movieInfo[i].popularity}</h3>
+                <h3>Description:${movieInfo[i].overview}</h3>`
+            );
+
+        };
 
     }; // onload handler
     
-    $('#movieResults').empty() // remove previous results 
+    $('#movieResults').empty() // remove previous results when next search applied - DO NOT USE INSIDE THE LOOP OR IT WILL ALWAYS EMPTY THE SEARCH EXCEPT FOR THE LAST RESULT.
 
+    // tell which URL to open and which method to use
     xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=${movie}`);
 
     xhr.send(); // actually send the request
@@ -39,14 +48,22 @@ $( function() {
 
     $('#searchButton').on('click', function(){
 
+        // on click function for search
         const movieSearch = $('#movieQuery').val(); // gets value typed into the form
 
         console.log(`Button clicked! Movie is:`, movieSearch);
 
         lookupMovie( movieSearch ); // invoke lookupMovie, perform request
 
-    })
+    });
 
-})
-console.log($('#movieQuery'));
+    // click enter function for search
+    $('#movieQuery').on('return',function (){
+       
+       
+        const movieSearch = $('#movieQuery').val();
+        lookupMovie( movieSearch );
 
+    });
+
+});
