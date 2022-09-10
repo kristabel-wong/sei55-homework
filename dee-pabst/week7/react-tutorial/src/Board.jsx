8ee5e50c59900e3d7c876650a6e28675de67985e
Dee Pabst <dee@canva.com>
2022-09-10 11:32:08 +1000

    added turn based logic and winner message

--- dee-pabst/week7/react-tutorial/src/Board.jsx
+++ dee-pabst/week7/react-tutorial/src/Board.jsx
@@ -1,29 +1,62 @@
 import React from "react";
 import Square from "./Square";
 
+function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++){
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 class Board extends React.Component {
 
     state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
 
     renderSquare(i) {
         return (
-            <Square 
+            <Square
                 value={this.state.squares[i]}
-                onClick={()=>this.handleClick(i)}
+                onClick={() => this.handleClick(i)}
             />
         );
     }
-    
+
     render() {
-        const status = 'Next Player: X'
+
+        const winner = calculateWinner(this.state.squares);
+        
+        let status;
+        if (winner){
+            status = 'Winner: ' + winner;
+        } else {
+           status  = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
