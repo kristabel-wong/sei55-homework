a0e2d78ebc8f17a5e42d296583a8faabda091fe3
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-12 23:48:28 +1000

    react flickr search homework

--- craig-rolleston/week8/mon/react-flickr-search/src/components/Home.jsx
+++ craig-rolleston/week8/mon/react-flickr-search/src/components/Home.jsx
@@ -4,14 +4,17 @@ import React from "react";
 class Home extends React.Component {
 
     render(){
+        
         return (
+
             <div>
                 <h2>Home</h2>
-                <h3><em>Welcome to Flickr. Search and Explore</em></h3>
+                <h3><em>Welcome to Flickr! Search and Explore</em></h3>
                 <br />
                 <img className="homeimage" src="https://i.pinimg.com/originals/00/2b/b1/002bb1c5aa0d0d36bb44585d1cd10cfb.jpg" alt="tropical paradise" />
                 <br />
             </div>
+
         );
 
     }
--- craig-rolleston/week8/mon/react-flickr-search/src/components/Results.jsx
+++ craig-rolleston/week8/mon/react-flickr-search/src/components/Results.jsx
@@ -1,16 +1,67 @@
 
 import React from 'react';
 
+import axios from 'axios';
 
-// const generateImageUrl = (p) => {
-//     return `https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}_q.jpg`;
-// }; // generateImageUrl()
+
+const FLICKR_BASE_URL = 'https://www.flickr.com/services/rest/';
+const FLICKR_API_KEY  = '2f5ac274ecfac5a455f38745704ad084';
+
+const generateImageUrl = (p) => {
+    return `https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}_q.jpg`;
+}; // generateImageUrl()
 
 class Results extends React.Component {
 
+    state = {
+        
+        resultPhotos: [], // the actual response data
+        loading: true, // controls whether or not to show loading message
+        error: null, // whether or not to show error message
+        
+    };
+    
+    performSearch = (query) => {
+
+        console.log('FlickrSearch::performSearch()', query);
+
+        // When you convert this into a Router-based project,
+        // the search results page should be a distinct route,
+        // which means as soon as it is mounted, you
+        // should perform a search... i.e., the axios.get()
+        // should be triggered within componentDidMount()
+        
+        axios.get( FLICKR_BASE_URL, {
+            
+            params: {
+              // These key-value pairs turn into querystring &key=value pairs
+              method: 'flickr.photos.search',
+              api_key: FLICKR_API_KEY,
+              format: 'json',
+              nojsoncallback: 1,
+              text: query
+            }
+        
+        })
+        .then( res => {
+            console.log( 'response', res.data );
+            console.log('test');
+            
+            this.setState({
+                resultPhotos: res.data.photos.photo,
+                loading: false,
+            })
+        })
+        .catch( err => {
+            console.log( 'Error loading results from API:', err );
+            this.setState({ error: err });
+        });
+
+    }
 
     componentDidMount(){
         
+        this.performSearch(this.props.match.params.query);
         // THIS IS THE PLACE TO LOAD API DATA
         console.log('componentDidMount() Loading data with query:', this.props.match.params.query);
         
@@ -30,9 +81,8 @@ class Results extends React.Component {
 
                 <hr /> */}
 
-
-
-                {/* {this.state.resultPhotos.map( p => <img key={p.id} src={generateImageUrl(p)} alt={ p.title } /> )} */}
+                {this.state.resultPhotos.map( p => <img key={p.id} src={generateImageUrl(p)} alt={ p.title } /> )}
+                
             </div>
             
             
@@ -42,3 +92,4 @@ class Results extends React.Component {
 } // class Home
 
 export default Results;
+
